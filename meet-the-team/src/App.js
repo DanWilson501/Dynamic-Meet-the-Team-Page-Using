98635f6d5eb7656bbef920cce0f45b72651e4f67
Daniel Wilson <danwilson501@gmail.com>
2024-07-09 15:15:32 -0400

    header component integrated

--- meet-the-team/src/App.js
+++ meet-the-team/src/App.js
@@ -4,10 +4,12 @@ import React from 'react';
 import './App.css';
 import Gallery from './components/Gallery';
 import teamMembers from './data';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Gallery teamMembers={teamMembers} />
     </div>
   );
