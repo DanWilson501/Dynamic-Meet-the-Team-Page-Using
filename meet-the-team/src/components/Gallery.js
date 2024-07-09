e07823d80a13abd777defd1715a534b8d669ad99
Daniel Wilson <danwilson501@gmail.com>
2024-07-09 14:08:36 -0400

    develop profile and gallery components to import into app.js

---
+++ meet-the-team/src/components/Gallery.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Profile from './Profile';
+
+function Gallery({ teamMembers }) {
+  return (
+    <div className="gallery">
+      {teamMembers.map((member, index) => (
+        <Profile key={index} member={member} />
+      ))}
+    </div>
+  );
+}
+
+export default Gallery;
